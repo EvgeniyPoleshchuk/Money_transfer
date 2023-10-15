b2fd6d4d1a67c4c7642df7326e0a919986e04787
EvgeniyPoleshchuk <123636823+EvgeniyPoleshchuk@users.noreply.github.com>
2023-10-15 11:13:04 +0300

    Update ConfirmOperationException.java

--- src/main/java/ru/netology/exception/ConfirmOperationException.java
+++ src/main/java/ru/netology/exception/ConfirmOperationException.java
@@ -1,8 +1,6 @@
 package ru.netology.exception;
 
 
-
-
 public class ConfirmOperationException extends RuntimeException{
     public ConfirmOperationException(String message) {
         super(message);
